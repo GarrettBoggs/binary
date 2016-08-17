96b33c304d9b50ad5c9917d832816993a524cf15
bobby crowston and garrett boggs <garrett@>
2016-08-17 14:59:26 -0700

    add hexidecimal program

---
+++ README.md
@@ -0,0 +1,8 @@
+#Binary Converter
+
+**This should return a decimal when you enter a binary**
+*Example Input:* _10_
+*Example Output:* _2_
+**This should return a decimal when you enter a Hexadecimal**
+*Example Input:* _a_
+*Example Output:* _10_
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 // Business Logic
 var total = 0;
-var convert = function(data){
+var counter;
+var hexvalues = ["a","b","c","d","e","f"];
+var value = 0;
+var digit = 0;
+
+var binaryConvert = function(data)
+{
+  total = 0;
   if(data === "0")
   {
     return 0;
@@ -10,21 +17,47 @@ var convert = function(data){
     return 1;
   }
 
-  for(var i = (data.length-1); i > 0; i -= 1)
+  for(var i = 0, counter = data.length; i < data.length; i++)
   {
-    total = total + (parseInt(data.charAt(i)) * Math.pow(2,i));
-    console.log(total);
+    console.log("Int" + parseInt(data.charAt(i)) + "power" + Math.pow(2,counter));
+    total = total + (parseInt(data.charAt(i)) * Math.pow(2,counter));
+    counter -= 1;
   }
 
   return total;
+}
 
+var hexaConvert = function(data){
+  total = 0;
+  for(var i = 0, counter = (data.length-1); i < data.length; i++)
+  {
+    for(var j = 0; j < hexvalues.length ; j++ )
+    {
+      if(data.charAt(i) === hexvalues[j])
+      {
+        value = 10 + j;
+      }
+    }
+    if(value === 0)
+    {
+      digit = parseInt(data.charAt(i));
+    }
+    console.log("Int" + (value+digit) + "power" + Math.pow(16,counter));
+    total = total + ((digit + value) * Math.pow(16,counter));
+    counter -= 1;
+    digit = 0;
+  }
+  return total;
 }
 //User interface
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var yearInput = $("#year").val();
+    var binInput = $("#bin").val();
+    var hexInput = $("#hex").val();
 
-    $("#output").text("This is a leap year " + convert(yearInput));
+    $("#output").text("Binary to Decimal: " + binaryConvert(binInput));
+
+    $("#output2").text("Hexadecimal to Decimal: " + hexaConvert(hexInput));
   });
 });
